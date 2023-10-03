3ded3d880b8f44cdd77b3b58b6769c90f8085ab4
VERTIGOJACK <vertigo.digital.design@gmail.com>
2023-10-03 15:10:39 +0200

    added button and logic for setting zero point

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
 import {
+  Button,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -9,31 +10,21 @@ import {
 } from "react-native";
 import { Accelerometer } from "expo-sensors";
 
+//ball dimensions for calc
+const ballSize = 30;
+
 export default function App() {
-  // deconstructs x y from setdata input
-  const [{ x, y }, setData] = useState({
+  // deconstructs x and y from input
+  const [accelerometer, setAccelerometer] = useState({
     x: 0,
     y: 0,
+    z: 0,
   });
-
-  //state for subbing to accelerometer
-  const [subscription, setSubscription] = useState(null);
-
-  //need phone dimensions to find center
-  const windowDimensions = useWindowDimensions();
 
   const _subscribe = () => {
     setSubscription(
       Accelerometer.addListener((measurement) => {
-        let center = {
-          x: windowDimensions.width / 2,
-          y: windowDimensions.height / 2,
-        };
-
-        let position = { x: 0, y: 0 };
-        position.x = center.x + measurement.x * center.x;
-        position.y = center.y + measurement.y * center.y;
-        setData(position);
+        setAccelerometer(measurement);
       })
     );
   };
@@ -49,26 +40,90 @@ export default function App() {
     return () => _unsubscribe();
   }, []);
 
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  //need phone dimensions to find center
+  const windowDimensions = useWindowDimensions();
+
+  useEffect(() => {
+    let centerOfView = {
+      x: windowDimensions.width / 2,
+      y: windowDimensions.height / 2,
+    };
+
+    let position = { x: 0, y: 0 };
+
+    position.x =
+      centerOfView.x +
+      (accelerometer.x - zeroPoint.x) * centerOfView.x -
+      ballSize;
+
+    position.y =
+      centerOfView.y +
+      (accelerometer.y - zeroPoint.y) * centerOfView.y -
+      ballSize;
+
+    setPosition(position);
+  }, [accelerometer]);
+
+  const [zeroPoint, setZeroPoint] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const handleZeroPoint = () => {
+    setZeroPoint(accelerometer);
+  };
+
+  const [viewSize, setViewSize] = useState({ width: 0, height: 0 });
+
+  //state for subbing to accelerometer
+  const [subscription, setSubscription] = useState(null);
+
   return (
-    <View>
-      <StatusBar></StatusBar>
+    <View style={styles.container}>
+      {/* <StatusBar></StatusBar> */}
       <View style={styles.container}>
-        <View style={{ ...styles.bubble, left: x, top: y }}></View>
+        <View style={styles.circle}></View>
+        <View
+          style={{
+            ...styles.bubble,
+            left: position.x,
+            top: position.y,
+          }}></View>
       </View>
+      <Button title="Zero" onPress={handleZeroPoint}></Button>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    position: "relative",
+    flexDirection: "column",
+    width: "100%",
+    height: "100%",
     flex: 1,
     paddingHorizontal: 20,
+    justifyContent: "center",
+    alignItems: "center",
   },
   bubble: {
     position: "absolute",
-    width: 10,
-    height: 10,
+    width: ballSize,
+    height: ballSize,
     backgroundColor: "red",
+    borderRadius: 100,
+  },
+  circle: {
+    position: "relative",
+    width: 50,
+    height: 50,
+    borderWidth: 5,
+    borderStyle: "solid",
+    borderColor: "red",
+    borderRadius: 100,
   },
 });
